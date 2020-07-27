c8848ffc5ca4de177e5bc144f3d30a6dcb1b7f84
Damflex <akanji4destiny@gmail.com>
2020-07-28 00:28:59 +0100

    MyName.vue

---
+++ src/components/MyName.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="center">
+         <h1>Hello! My name is <br> <strong> {{ name }} </strong>. </h1>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'MyName',
+    props: ['name']
+}
+</script>
+
+<style>
+    .center {
+        font-weight:300;
+        justify-content: center;
+        align-items: center;
+    }
+    h1 {
+        font-weight:200;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
